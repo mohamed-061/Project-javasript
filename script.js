c1d030e54172e6baeec63d19c865a67871193afb
mohd ahmed <143282929+mohamed-061@users.noreply.github.com>
2023-10-01 21:20:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to generate a name tag
+
 function generateName() {
     const nameInput = document.getElementById('nameInput').value;
     if (nameInput.trim() !== '') {
@@ -7,7 +7,7 @@ function generateName() {
         tag.classList.add('tag');
         tag.innerHTML = nameInput;
         
-        // Add delete icon to the tag
+    
         const deleteIcon = document.createElement('i');
         deleteIcon.classList.add('fa', 'fa-trash');
         deleteIcon.onclick = function() {
@@ -17,21 +17,21 @@ function generateName() {
         
         tagsContainer.appendChild(tag);
         
-        // Clear the input field
+     
         document.getElementById('nameInput').value = '';
         
-        // Generate a random background color
+  
         const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         tag.style.backgroundColor = randomColor;
     }
 }
 
-// Function to clear all tags
+
 function clearAllTags() {
     const tagsContainer = document.getElementById('tagsContainer');
     tagsContainer.innerHTML = '';
 }
 
-// Add click event listeners to the buttons
+
 document.getElementById('generateButton').addEventListener('click', generateName);
 document.getElementById('clearAllButton').addEventListener('click', clearAllTags);
